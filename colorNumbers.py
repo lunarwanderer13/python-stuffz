812c086385a3a169a18aed6b8fb2c9a794c8a66c
LunarWanderer月 <76808845+lunarwanderer13@users.noreply.github.com>
2024-06-22 21:58:33 +0200

    Upload colorNumbers.py

---
+++ colorNumbers.py
@@ -0,0 +1,25 @@
+import time
+import random
+from colored import fg, attr
+import pyfiglet
+import keyboard
+
+num: int = 0
+limit: int = 999999999
+
+def main() -> None:
+    for num in range(limit):
+        num += 1
+        col: int = random.randint(1,231)
+
+        # Printing num's ascii
+        ascii = pyfiglet.figlet_format(str(num))
+        print(f'{fg(col)}{ascii}{attr(0)}')
+
+        time.sleep(.5)
+
+if __name__ == '__main__':
+    main()
+    # Ending the program
+    print(f'{fg(1)}Reached the limit of {limit}{attr(0)}')
+    keyboard.wait('shift + esc', suppress=True)
